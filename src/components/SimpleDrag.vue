e5eed894d6a2b1408eb589e6db37161ea16b3a59
tvshreyanshi <shreyanshi@techvoot.com>
2023-09-22 13:24:57 +0530

    drag between multiple array dynamic

--- src/components/SimpleDrag.vue
+++ src/components/SimpleDrag.vue
@@ -3,45 +3,26 @@
     <div
       class="card shadow-lg p-3 mb-5 bg-body rounded m-2"
       style="width: 18rem"
+      v-for="(data, index) in response.data"
+      :key="index"
     >
-      <h5 class="card-title">Array1</h5>
+      <h5 class="card-title">{{data.title}}</h5>
       <div class="h-100">
         <ul
-          @drop.prevent="dropItem('array1', $event)"
+          @drop.prevent="dropItem(index, $event)"
           @dragover.prevent="allowDrop"
           class="list-group list-group-flush h-100"
         >
           <li
             class="list-group-item"
-            v-for="(data, index) in array1"
-            :key="index"
+            v-for="(item, itemindex) in data.data"
+            :key="itemindex"
             :draggable="true"
-            @dragstart="dragStart('array1', index)"
+            @dragstart="dragStart(index, itemindex)"
+            @dragover.prevent="internalAllowDrop"
+            @drop.prevent="internalDrop(index, itemindex)"
           >
-            {{ data }}
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div
-      class="card shadow-lg p-3 mb-5 bg-body rounded m-2"
-      style="width: 18rem"
-    >
-      <h5 class="card-title">Array2</h5>
-      <div class="h-100">
-        <ul
-          @drop.prevent="dropItem('array2', $event)"
-          @dragover.prevent="allowDrop"
-          class="list-group list-group-flush h-100"
-        >
-          <li
-            v-for="(data, index) in array2"
-            :key="index"
-            class="list-group-item"
-            draggable="true"
-            @dragstart="dragStart('array2', index)"
-          >
-            {{ data }}
+            {{ item.name }}
           </li>
         </ul>
       </div>
@@ -53,44 +34,62 @@ import { ref } from "vue";
 export default {
   name: "SimpleDrag",
   setup() {
-    const array1 = ref(["Item 1", "Item 2", "Item 3"]);
-    const array2 = ref(["Item 4", "Item 5", "Item 6"]);
+    const response = ref({"data": [
+          {
+            title:'Array 1',
+            data:[{ name: "AA", age: 11 }, { name: "BB", age: 22 }, { name: "CC", age: 33 },]
+          },
+          {
+            title: 'Array 2',
+            data:[{ name: "XX", age: 66 }],
+          },
+          {
+            title: 'Array 3',
+            data: []
+          }
+    ] })
     const draggedItem = ref(null);
     const draggedArray = ref(null);
 
-    const dragStart = (arrayName, index) => {
-      draggedItem.value = index;
-      draggedArray.value = arrayName;
+    const dragStart = (arrayIndex, itemIndex) => {
+      draggedItem.value = itemIndex;
+      draggedArray.value = arrayIndex;
     };
     const allowDrop = (event) => {
       event.preventDefault();
     };
-    const dropItem = (targetArray, event) => {
+    const internalAllowDrop = (event) => {
+      event.preventDefault();
+    };
+    const internalDrop = (targetArrayIndex, targetIndex) => {
       if (draggedItem.value === null || draggedArray.value === null) return;
 
+      const targetArray = response.value.data[targetArrayIndex];
+      const draggedItemObj = targetArray.data.splice(draggedItem.value, 1)[0];
+      targetArray.data.splice(targetIndex, 0, draggedItemObj);
+
+      draggedItem.value = null;
+      draggedArray.value = null;
+    };
+    const dropItem = (targetArrayIndex, event) => {
+      if (draggedItem.value === null || draggedArray.value === null) return;
+
+      const targetArray = response.value.data[targetArrayIndex];
       const targetList = event.target.parentElement;
       const targetIndex = Array.from(targetList.children).indexOf(event.target);
       if (targetIndex === targetList.children.length - 1) {
-        if (targetArray === "array1" && draggedArray.value === "array2") {
-          array1.value.push(array2.value[draggedItem.value]);
-          array2.value.splice(draggedItem.value, 1);
-        } else if (
-          targetArray === "array2" &&
-          draggedArray.value === "array1"
-        ) {
-          array2.value.push(array1.value[draggedItem.value]);
-          array1.value.splice(draggedItem.value, 1);
+        const sourceArrayIndex = draggedArray.value;
+        if (sourceArrayIndex !== targetArrayIndex) {
+          const sourceArray = response.value.data[sourceArrayIndex];
+          targetArray.data.push(sourceArray.data[draggedItem.value]);
+          sourceArray.data.splice(draggedItem.value, 1);
         }
       } else {
-        if (targetArray === "array1" && draggedArray.value === "array2") {
-          array1.value.splice(targetIndex, 0, array2.value[draggedItem.value]);
-          array2.value.splice(draggedItem.value, 1);
-        } else if (
-          targetArray === "array2" &&
-          draggedArray.value === "array1"
-        ) {
-          array2.value.splice(targetIndex, 0, array1.value[draggedItem.value]);
-          array1.value.splice(draggedItem.value, 1);
+        const sourceArrayIndex = draggedArray.value;
+        if (sourceArrayIndex !== targetArrayIndex) {
+          const sourceArray = response.value.data[sourceArrayIndex];
+          targetArray.data.splice(targetIndex, 0, sourceArray.data[draggedItem.value]);
+          sourceArray.data.splice(draggedItem.value, 1);
         }
       }
 
@@ -98,11 +97,12 @@ export default {
       draggedArray.value = null;
     };
     return {
-      array1,
-      array2,
+      response,
       dragStart,
       allowDrop,
       dropItem,
+      internalDrop,
+      internalAllowDrop,
     };
   },
 };
